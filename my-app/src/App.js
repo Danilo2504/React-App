3a3041dad2f4fce09a73874799e1e07b2b315e0d
Dani2504 <danilobautista2004@gmail.com>
2022-07-05 11:38:49 -0300

    third commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Calculator from './components/Calculator';
 // import InputKids from './components/InputKids';
+// import InputKids from './components/CrudApp';
 
 function App() {
   return(
   // <InputKids/>
+  // <CurdApp/>
   <Calculator/>
   );
 }
